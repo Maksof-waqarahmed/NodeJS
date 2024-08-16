bf27b5db5e8dd224947624e728ae78b7f1b16bb6
Waqar Ahmed <160152452+Maksof-waqarahmed@users.noreply.github.com>
2024-08-16 18:46:58 +0500

    Add files via upload

---
+++ 4) Package.JSON/package.js
@@ -0,0 +1,13 @@
+// pkg.json: priject sy related information apny pas rakhta h. like proejct Name, version, git repository, command, pkg etc.
+// is file ko banay k lye terminal par "npm init" chalao.
+
+//npm: Node Pachage Manager (External Pkg/libaray ko manage krta h);
+
+// package-Lock.JSON: package sy related information save krti h
+
+var colors = require('colors');
+ 
+console.log('hello'.green); 
+
+//node Module jb ap koi pkg install krty ho to by default install hti h is k ander us depency k related chezy hoti h isko github par push nh krna chiaye bcz size zeuada hota h or jb be agr ye delete ho jaye to ap "npm install" run kr dena sb khud sy install ho jaye gi.
+//.gitignore: jisko par push nh krna chatty uska name is file me likh do "/node-module"
