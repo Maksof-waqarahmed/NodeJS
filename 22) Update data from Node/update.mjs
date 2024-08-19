9db7ff3c6e3de0020c99731ee03fb87e87099677
Waqar Ahmed <160152452+Maksof-waqarahmed@users.noreply.github.com>
2024-08-19 10:40:02 +0500

    Add files via upload

---
+++ 22) Update data from Node/update.mjs
@@ -0,0 +1,13 @@
+import {connectDB} from './mongoDBConnect.mjs'
+
+const updateData = async () => {
+    let data = await connectDB();
+    let result = data.updateMany( // is k ander hamesha 2 obj d gy k knsa chnage krna h or kia chnage krna h
+        { des: "Developer" },
+        { $set: {loc: "Karachi"} } // new prop be add kr skty hen
+        // { $set: {name: "Raza"} }
+    )
+    console.log("Result ==> ",await result);
+}
+updateData() // agr bar bar chalo gy to ye ek he bar data update kry ga
+//agr same name k obj h to or ap uska name sy condition laga kr kuch chnage kr rhy h UpdateOne kr k to phela wala update hoga
