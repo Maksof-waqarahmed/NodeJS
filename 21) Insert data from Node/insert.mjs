bdb71d53dc87e34db60351eae2078dc6eac88048
Waqar Ahmed <160152452+Maksof-waqarahmed@users.noreply.github.com>
2024-08-19 10:39:15 +0500

    Add files via upload

---
+++ 21) Insert data from Node/insert.mjs
@@ -0,0 +1,38 @@
+import {connectDB} from './mongoDBConnect.mjs'
+const insertData = async () => {
+    let db = await connectDB();
+    // const result = await db.insertOne(
+    //     {
+    //         name: "Waqar",
+    //         age: 22,
+    //         email: "waqar@gmail.com",
+    //         des: "Developer"
+    //     }
+    // );
+
+    const result = await db.insertMany(
+        [
+            {
+                name: "Waqar",
+                age: 22,
+                email: "waqar@gmail.com",
+                des: "Developer"
+            },
+            {
+                name: "Ahad",
+                age: 22,
+                email: "ahad@gmail.com",
+                des: "Developer"
+            },
+            {
+                name: "Ahmed",
+                age: 22,
+                email: "ahmed@gmail.com",
+                des: "Developer"
+            },
+            
+        ]
+    );
+    if(result.acknowledged) console.log("Insert", result);
+}
+insertData()
---
+++ 21) Insert data from Node/read.mjs
@@ -0,0 +1,38 @@
+// import {MongoClient} from 'mongodb';
+// const url = 'mongodb://localhost:27017';
+// const client = new MongoClient(url);
+
+// const database = 'e-commerce';
+
+// //just Db connect k lye ye function banaya h
+// async function dbConnect(){
+//     let result = await client.connect(); 
+//     let db = result.db(database)
+//     return db.collection('products'); // promise return kry ga
+// }
+
+// //read data from DB through .then
+// // dbConnect().then((res) => {
+// //     res.find({name: "coreI9"}).toArray().then((data) => {
+// //         console.log("Data" , data)
+// //     }) // single data k lye ye laga do wrna hata lo
+// // });
+
+// //read data from DB through async/await
+// const getData = async () => {
+//     let data = await dbConnect();
+//     data = await data.find().toArray();
+//     console.log("data",data);
+// }
+// getData()
+
+//alag file sy kesy kry gy
+
+import {connectDB} from './mongoDBConnect.mjs';
+//read data from DB through async/await
+const getData = async () => {
+    let data = await connectDB();
+    data = await data.find().toArray();
+    console.log("data",data);
+}
+getData()
